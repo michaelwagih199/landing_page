d4030ccf86787fad70f321a529dea1325d76c8b2
Michael Wagih Boules | Ejada Systems Ltd <mwagih@ejadacairo.local>
2023-05-18 16:40:43 +0200

    update some valu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,21 @@
-console.log('Hello World');
+
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+
+btnHamburger.addEventListener('click', () => {
+    
+    console.log('open hamburger');
+    //close Humberge
+    if (header.classList.contains('open')){
+        header.classList.remove('open');
+        overlay.classList.add('fade-out');
+        overlay.classList.remove('fade-in');
+    }
+    else{//open
+        header.classList.add('open');
+        overlay.classList.add('fade-in');
+        overlay.classList.remove('fade-out');
+    }
+
+})
